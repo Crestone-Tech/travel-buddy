b27a111becce22a979af15d15cf3da9545cfd2e5
JeffMichael <jmpresto1@yahoo.com>
2024-08-08 16:04:05 -0400

    basic login form complete (minimal functionality, no true 'submission' yet

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,1 +1,30 @@
 import React from "react";
+
+const Login = () => {
+    return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
