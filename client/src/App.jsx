3957816a2ca73e76ff51ed840525b80476d91921
JeffMichael <jmpresto1@yahoo.com>
2024-08-08 14:13:56 -0400

    react router to component elements added to main.jsx in client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
@@ -11,7 +11,7 @@ function App() {
       <Header><NavBar /></Header>
 
       <main>
-
+        <Outlet />
       </main>
       <Footer />
     </>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Envision />,
+      },
+      {
+        path: '/plan',
+        element: <Plan />,
+      },
+      {
+        path: '/tribes',
+        element: <Tribes />,
+      },
+    ],
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
