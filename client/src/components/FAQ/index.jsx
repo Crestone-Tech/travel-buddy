c2274f904f8bf1698e28d3ee7bcc75118b84f1e3
JohnnyBaldyga <jonathanbaldyga@gmail.com>
2024-08-08 14:26:37 -0400

    add base question template to FAQ

--- client/src/components/FAQ/index.jsx
+++ client/src/components/FAQ/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 export default function FAQ() {
+  const questions = [
+    {
+      question: "What is Travel Buddy?",
+      answer:
+        "Travel Buddy is the Ultimate travel tool for you and your tribe.etc...",
+    },
+    { question: "Why make Travel buddy?", answer: "Why not...." },
+    { question: "", answer: "" },
+  ];
   return (
     <div>
       <h2> Frequently Asked Questions</h2>
