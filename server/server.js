bf2a11345aed00a1ff57313214aef3e84933799a
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-08 10:45:27 -0700

    removed type:module from package.json files

---
+++ server/server.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
