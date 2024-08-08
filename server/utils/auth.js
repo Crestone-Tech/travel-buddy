13949cbffaaf9548af7e15a744b1072cdddc076e
flcn <jfalconpriv@gmail.com>
2024-08-08 11:23:55 -0500

    added auth.js, authMiddleware function

---
+++ server/utils/auth.js
@@ -0,0 +1,36 @@
+// PURPOSE: authentication middleware
+
+const jwt = require("jsonwebtoken");
+
+const secret = "secret_travel_buddy"; // secret key
+const expiration = "2h"; // 2 hours
+
+module.exports = {
+  // auth middleware function
+  authMiddleware: function (req, res, next) {
+    // token sending in req query or headers
+    let token = req.query.token || req.headers.authorization;
+
+    // if token in headers, adjust token value
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    // if no token, return message
+    if (!token) {
+      return res.status(400).json({ message: "You have no token!" });
+    }
+
+    // verify token
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+      return res.status(400).json({ message: "invalid token!" });
+    }
+
+    // next middleware
+    next();
+  },
+};
