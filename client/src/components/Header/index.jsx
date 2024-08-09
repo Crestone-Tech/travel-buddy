aaf2c448afaee808112b799697ab8f1286b27726
JohnnyBaldyga <jonathanbaldyga@gmail.com>
2024-08-09 11:36:17 -0400

    add ul pf contact faq signup and login to header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -4,6 +4,22 @@ export default function Header(props) {
   return (
     <header>
       <img src={travelbuddy} alt="Logo" />
+      <nav className="nav">
+        <ul className="nav-list">
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#faq">FAQ</a>
+          </li>
+          <li>
+            <a href="#login">Login</a>
+          </li>
+          <li>
+            <a href="#signup">Signup</a>
+          </li>
+        </ul>
+      </nav>
       {props.children}
     </header>
   );
