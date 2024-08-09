4df33484fc7c1cce9131a73a1df9d4be6c8b3fc8
JohnnyBaldyga <jonathanbaldyga@gmail.com>
2024-08-09 18:24:31 -0400

    imports

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
 export default function NavBar() {
   const currentPage = useLocation.pathname;
