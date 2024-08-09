a1cbca640f7b67d5ee906206e80c1323bc0dad9b
JohnnyBaldyga <jonathanbaldyga@gmail.com>
2024-08-09 12:22:51 -0400

    changed index js to jsx

--- client/src/components/Hero/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import traveldreams from "../../assets/images/traveldreams.png";
-import cenote from "../../assets/images/cenote.png";
-
-export default function Hero(props) {
-  return <section></section>;
-}
