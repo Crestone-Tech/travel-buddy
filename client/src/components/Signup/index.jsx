b659966a3438e97d87cecab8f5866c48465a3c37
JeffMichael <jmpresto1@yahoo.com>
2024-08-09 11:54:49 -0400

    Sign-up form not fully functional but there

--- client/src/components/Signup/index.jsx
+++ client/src/components/Signup/index.jsx
@@ -2,7 +2,52 @@ import React from "react";
 
 const Signup = () => {
     return (
-    <p>This is a signup page</p>
+      <div className="signup-container">
+      <h2>Sign up</h2>
+      <form>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            id="firstName"
+            // value={firstName}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            id="lastName"
+            // value={lastName}
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            // value={username}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            // value={password}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            // value={confirmPassword}
+          />
+        </div>
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
   );
 }
 
