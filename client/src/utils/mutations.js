76ab5f4cdec30ca3796fcbb38b9993dc4eb39d81
flcn <jfalconpriv@gmail.com>
2024-08-09 11:07:36 -0500

    added client mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,29 @@
+// PURPOSE: to hold the mutations for the application
+
+import { gql } from "@apollo/client";
+
+// ADD_USER mutation
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// LOGIN_USER mutation
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
