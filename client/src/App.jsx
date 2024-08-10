8bc80ebb4ded70c83a84e44df2a484cf23169edf
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-09 17:32:23 -0700

    adds proxy server on port 3000 and apollo provider to client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,30 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import NavBar from './components/NavBar'
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
-
   return (
-    <>
-      <Header><NavBar /></Header>
+    <ApolloProvider client={client}>
+      <Header>
+        <NavBar />
+      </Header>
 
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
-  )
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
