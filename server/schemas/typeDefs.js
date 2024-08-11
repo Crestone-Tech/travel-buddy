f6818535c4a53f3c8c927490c33d9009682b4f54
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-11 09:39:25 -0700

    added Reservation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,44 @@
+const { GraphQLScalarType, Kind } = require("graphql");
+
+const dateScalar = new GraphQLScalarType({
+  name: "Date",
+  description: "Date custom scalar type",
+  serialize(value) {
+    return value.getTime(); // Convert outgoing Date to integer for JSON
+  },
+  parseValue(value) {
+    return new Date(value); // Convert incoming integer to Date
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST literal to Date
+    }
+    return null;
+  },
+});
+
 const typeDefs = `
+  scalar Date
+
   type User {
     id: ID!
     name: String!
     email: String!
     password: String!
+  }
+
+  type Reservation {
+    id: ID!
+    title: String!
+    category: String!
+    description: String
+    startDate: Date!
+    endDate: Date
+    status: String!
+    provider: String
+    transportationType: String
+    price: Float
+    priceCurrency: String
   }
 
   type Auth {
@@ -12,6 +47,7 @@ const typeDefs = `
   }
 
   type Query {
+  reservation(_id: ID!): Reservation
   users: [User]
   user(_id: ID!): User
   me: User
