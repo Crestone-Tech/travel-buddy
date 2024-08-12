1554fac4d494927382e83dc848bfdb10703c6415
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-12 08:05:07 -0700

    removed experimental theme config object

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,33 +15,7 @@ import Plan from "./pages/Plan";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 
 // used to add themes for MUI
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#1976d2",
-    },
-    success: {
-      main: "#4caf50",
-    },
-  },
-  typography: {
-    fontSize: 16,
-    h3: {
-      fontWeight: 700,
-      fontSize: "2.2rem",
-    },
-    h4: {
-      fontWeight: 700,
-      fontSize: "1.75rem",
-    },
-    h5: {
-      fontWeight: 500,
-    },
-    h6: {
-      fontWeight: 500,
-    },
-  },
-});
+const theme = createTheme();
 
 // used to connect to the GraphQL API server
 const httpLink = createHttpLink({
