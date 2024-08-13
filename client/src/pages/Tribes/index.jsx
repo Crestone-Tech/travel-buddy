e54930a96bd41db418f9da23352852793a2280c7
Arcaneghost24 <jmpresto1@yahoo.com>
2024-08-13 11:36:58 -0400

    Buttons on page, successfully console.log

--- client/src/pages/Tribes/index.jsx
+++ client/src/pages/Tribes/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 export default function Tribes() {
-    return (
-        <div>
-            Tribes Page!
-        </div>
-    )
+  return (
+    <div>
+      Tribes Page!
+      <button
+        id="tribeInit"
+        onClick={() => console.log("Will later create a tribe")}
+      >
+        Start My Tribe
+      </button>
+      <button
+        id="tribeDelete"
+        onClick={() => console.log("Will later delete your tribe")}
+      >
+        Disband My Tribe
+      </button>
+    </div>
+  );
 }
