1493030d2fde4a214a3674fecdf946d8682f9d51
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-13 13:37:24 -0700

    adds open:true

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    open:true,
     proxy: {
       "/graphql": "http://localhost:3001",
     },
