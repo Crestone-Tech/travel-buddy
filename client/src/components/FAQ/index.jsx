d1f7d0a46f8672b1c8c9316d487ae352efda3da7
JohnnyBaldyga <jonathanbaldyga@gmail.com>
2024-08-13 13:57:18 -0400

    updated FAQ questions

--- client/src/components/FAQ/index.jsx
+++ client/src/components/FAQ/index.jsx
@@ -5,10 +5,14 @@ export default function FAQ() {
     {
       question: "What is Travel Buddy?",
       answer:
-        "Travel Buddy is the Ultimate travel tool for you and your tribe.etc...",
+        "Travel Buddy is the Ultimate travel tool for you and your 'tribe'. With Travel Buddy you can cllectively as a group plan you dream vacation with our tools. With our Envision Board you can simultaneously use our integrated white board to share ideas and collaborate. You can manae your travel groups within our Tribes tab and plan reservations within our Buddy Tab. ",
     },
-    { question: "Why make Travel buddy?", answer: "Why not...." },
-    { question: "", answer: "" },
+    {
+      question: "Why make Travel buddy?",
+      answer:
+        "We created Travel Buddy to revolutionize the way groups plan and experience travel. In today’s fast-paced world, coordinating a group trip can be challenging, from juggling everyone's preferences to organizing activities and reservations. We wanted to provide a platform that simplifies this process, allowing friends, families, and like-minded travelers to come together seamlessly.",
+    },
+    { question: "Having Issues?", answer: "Contact us via our Contact Tab!" },
   ];
   return (
     <div className="faq-container">
